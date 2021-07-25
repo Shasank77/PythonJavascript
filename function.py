54c3fb0da23939f1151b3499eae42627fb7a13b2
Shasank77 <ghimiresha@gmail.com>
2021-07-25 11:35:34 +1000

    flight

--- function.py
+++ function.py
@@ -2,4 +2,4 @@ def square(x):
     return x * x
 
     for i in range(5):
-        print(f"the square root of {i} is {square(i)}")
+        print(f"the square of {i} is {square(i)}")
--- sequences.py
+++ sequences.py
@@ -2,6 +2,7 @@ number =["one", "three", "five"]
 coordinateX = 1.0
 coordinateY = 2.0
 
+# tuple for the cordinates
 coordinate = (1.0, 2.0)
 
 
